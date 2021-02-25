96892489cb3e2b34d37018f9fe5724614bea1bf4
Akash Kumar Singh <akashsingh049811@gmail.com>
2021-02-25 14:16:16 +0530

    added another weight

--- incremental_smt.py
+++ incremental_smt.py
@@ -34,8 +34,8 @@ def safe_space (x_, y_, l, map):
 map = np.array ([[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
                  [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
                  [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
-                 [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
-                 [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
+                 [0.5,0.5,0.5,0.5,0.0,0.0,0.5,0.5,0.5,0.5],
+                 [0.5,0.5,0.5,0.5,0.0,0.0,0.5,0.5,0.5,0.5],
                  [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
                  [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
                  [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5],
@@ -48,7 +48,7 @@ print (map.shape)
 
 num_covered = 0
 total = map.shape[0] * map.shape[1]
-R = 2
+R = 4
 local_range = 1
 
 start_x = np.empty ((R,))
@@ -56,9 +56,13 @@ start_y = np.empty ((R,))
 
 start_x[0] = 0
 start_x[1] = 0
+start_x[2] = 2
+start_x[3] = 2
 
 start_y[0] = 0
 start_y[1] = 2
+start_y[2] = 0
+start_y[3] = 2
 
 dimension_x = map.shape[1]
 dimension_y = map.shape[0]
@@ -78,6 +82,11 @@ for r in range (R):
     f = open (filename, 'w+')
     files.append (f)
 
+motion_w = 1 # cost of taking each step
+old_w = 1 # reward of visiting old grid first
+visible_w = 1 # reward of covering visible grids which are near
+not_covered_w = 0 # cost of covering already covered grid
+
 while num_covered < total:
     for r in range (R):
         print ("robot " + str (r) + ":", start_x[r], start_y[r])
@@ -93,12 +102,13 @@ while num_covered < total:
     Y = [[Int("y_%s_%s" % (i, j)) for j in range(T)] for i in range(R)]
     P = [[Int("p_%s_%s" % (i, j)) for j in range(T)] for i in range(R)]
     S = [[Int("s_%s_%s" % (i, j)) for j in range(T)] for i in range(R)]
+    NC = [[Int("NC_%s_%s" % (i, j)) for j in range(T)] for i in range(R)]
 
     Re = [Int("re_%s" % (j)) for j in range(num_visible)]
 
     C = [[Int("c_%s_%s" % (i, j)) for j in range(T)] for i in range(R)]
 
-    s.add (total_c == Sum (Re + S[0] + S[1] + C[0] + C[1]))
+    s.add (total_c == Sum (Re + S[0] + S[1] + S[2] + S[3] + C[0] + C[1] + C[2] + C[3]))
 
     for r in range (R):
         s.add (And (X[r][0] == start_x[r], Y[r][0] == start_y[r]))
@@ -121,21 +131,27 @@ while num_covered < total:
             s.add (And (Y[r][t] < dimension_y, Y[r][t] >= 0))
 
             s.add (And (P[r][t] < 5, P[r][t] >= 0))
-            s.add (Or (C[r][t] == 3, C[r][t] == 5))
+            s.add (Or (C[r][t] == 3 * motion_w, C[r][t] == 5 * motion_w))
             s.add (And (S[r][t] >= 0, S[r][t] <= 32))
+            s.add (Or (NC[r][t] == not_covered_w, NC[r][t] == 1000))
 
     # motion primitives
     for r in range (R):
         for t in range (T-1):
-            s.add(Implies(P[r][t] == 0, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t], C[r][t] == 3))) # same
-            s.add(Implies(P[r][t] == 1, And(X[r][t+1] == X[r][t]+1, Y[r][t+1] == Y[r][t], C[r][t] == 5))) # right
-            s.add(Implies(P[r][t] == 2, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t]+1, C[r][t] == 5))) # up
-            s.add(Implies(P[r][t] == 3, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t]-1, C[r][t] == 5))) # down
-            s.add(Implies(P[r][t] == 4, And(X[r][t+1] == X[r][t]-1, Y[r][t+1] == Y[r][t], C[r][t] == 5))) # left
+            s.add(Implies(P[r][t] == 0, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t], C[r][t] == 3 * motion_w))) # same
+            s.add(Implies(P[r][t] == 1, And(X[r][t+1] == X[r][t]+1, Y[r][t+1] == Y[r][t], C[r][t] == 5 * motion_w))) # right
+            s.add(Implies(P[r][t] == 2, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t]+1, C[r][t] == 5 * motion_w))) # up
+            s.add(Implies(P[r][t] == 3, And(X[r][t+1] == X[r][t], Y[r][t+1] == Y[r][t]-1, C[r][t] == 5 * motion_w))) # down
+            s.add(Implies(P[r][t] == 4, And(X[r][t+1] == X[r][t]-1, Y[r][t+1] == Y[r][t], C[r][t] == 5 * motion_w))) # left
 
     # collision avoidance
     for t in range(0, T):
         s.add(Or(X[0][t] != X[1][t], Y[0][t] != Y[1][t]))
+        s.add(Or(X[0][t] != X[2][t], Y[0][t] != Y[2][t]))
+        s.add(Or(X[0][t] != X[3][t], Y[0][t] != Y[3][t]))
+        s.add(Or(X[1][t] != X[2][t], Y[1][t] != Y[2][t]))
+        s.add(Or(X[1][t] != X[3][t], Y[1][t] != Y[3][t]))
+        s.add(Or(X[2][t] != X[3][t], Y[2][t] != Y[3][t]))
 
     safe = []
     for r in range (R):
@@ -150,7 +166,7 @@ while num_covered < total:
     # cover as many visible space as possible
     count = 0
     for (x,y) in visible:
-        s.add (Implies (Or ([And (X[r][t] == x, Y[r][t] == y) for r in range (R) for t in range (T)]), Re[count] == 2 * count))
+        s.add (Implies (Or ([And (X[r][t] == x, Y[r][t] == y) for r in range (R) for t in range (T)]), Re[count] == old_w * count))
         s.add (Implies (Not (Or ([And (X[r][t] == x, Y[r][t] == y) for r in range (R) for t in range (T)])), Re[count] == 2 * num_visible))
         count += 1
 
@@ -161,8 +177,13 @@ while num_covered < total:
                 cost = 0
                 for i in range (num_visible):
                     cost += (abs (visible[i][0] - x) + abs (visible[i][1] - y))
-                cost = int (cost / num_visible)
+                cost = visible_w * int (cost / num_visible)
                 s.add (Implies (And (X[r][t] == x, Y[r][t] == y), S[r][t] == cost))
+
+    for r in range (R):
+        for t in range (T):
+            s.add (Implies (Or ([And (X[r][t] == x, Y[r][t] == y) for (x,y) in covered]), NC[r][t] == not_covered_w))
+            s.add (Implies (Not (Or ([And (X[r][t] == x, Y[r][t] == y) for (x,y) in covered])), NC[r][t] == 1000))
 
     h = s.minimize (total_c)
 
--- plot.py
+++ plot.py
@@ -21,17 +21,17 @@ def readInput (filename):
 
 (x0, y0) = readInput ('waypoints_0')
 (x1, y1) = readInput ('waypoints_1')
-# (x2, y2) = readInput ('robot2.plan')
-# (x3, y3) = readInput ('robot3.plan')
+(x2, y2) = readInput ('waypoints_2')
+(x3, y3) = readInput ('waypoints_3')
 
-# obsx = [2, 3, 1, 3, 1, 2]
-# obsy = [0, 0, 2, 2, 4, 4]
+obsx = [4, 4, 5, 5]
+obsy = [3, 4, 3, 4]
 
-initialx = [x0[0], x1[0]]
-initialy = [y0[0], y1[0]]
+initialx = [x0[0], x1[0], x2[0], x3[0]]
+initialy = [y0[0], y1[0], y2[0], y3[0]]
 
-plt.scatter (initialx, initialy, c=['r','b'])
-# plt.scatter (obsx, obsy, c=['k','k','k','k','k','k'])
+plt.scatter (initialx, initialy, c=['r','b','g','y'])
+plt.scatter (obsx, obsy, c=['k','k','k','k'])
 plt.ylim ((-1, 11))
 plt.xlim ((-1, 11))
 plt.pause (1)
@@ -39,8 +39,8 @@ plt.pause (1)
 for i in range (0, len (x0), 1):
     plt.plot (x0[i:i+2], y0[i:i+2], 'r--')
     plt.plot (x1[i:i+2], y1[i:i+2], 'b--')
-    # plt.plot (x2[i:i+2], y2[i:i+2], 'go-')
-    # plt.plot (x3[i:i+2], y3[i:i+2], 'yo-')
+    plt.plot (x2[i:i+2], y2[i:i+2], 'g--')
+    plt.plot (x3[i:i+2], y3[i:i+2], 'y--')
     plt.pause (0.1)
 
 plt.show ()

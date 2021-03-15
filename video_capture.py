4e6d4cdf8837c54117cec996f8b64a124ead5957
Akash Kumar Singh <akashsingh049811@gmail.com>
2021-03-15 22:30:38 +0530

    updated video_capture

--- video_capture.py
+++ video_capture.py
@@ -2,9 +2,25 @@
 
 import cv2
 import numpy as np
+import rospy
+import time
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+from laser_pkg.msg import ImagePose
+
+pose = PoseStamped ()
+
+def cb (data):
+    global pose
+    pose.pose = data.pose.pose
+
+rospy.init_node ('video_capture')
+pub = rospy.Publisher ('/image_pose', ImagePose, queue_size=1, latch=True)
+sub = rospy.Subscriber ('/mavros/global_position/local/adjusted', Odometry, cb, queue_size=10)
 
 url = "rtsp://192.168.43.1:8554/fpv_stream"
 cap = cv2.VideoCapture (url)
+bridge = CvBridge ()
 
 if (cap.isOpened () == False):
     print ("error in opening video stream")
@@ -13,7 +29,12 @@ while (cap.isOpened()):
     ret, frame = cap.read ()
     if (ret == True):
         cv2.imshow ('Frame', frame)
-
+        image = bridge.cv2_to_imgmsg (frame, encoding="bgr8")
+        msg = ImagePose ()
+        msg.pose = pose
+        msg.image = image
+        pub.publish (msg)
+        time.sleep (4)
         if (cv2.waitKey (25) & 0xFF == ord ('q')):
             break
     else:
